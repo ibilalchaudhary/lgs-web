0668412a80adb9e4d5dd1e7e75ec2b467ce4128d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-04-07 05:33:06 +0500

    pic change

--- src/screens/OurSchool.js
+++ src/screens/OurSchool.js
@@ -139,7 +139,7 @@ export default function OurSchool() {
         <div className="from__the__pricipal__container__wrapper">
           <div className="from__the__pricipal__container">
             <img
-              src={director_pic}
+              src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2MCA2MCI+PGRlZnM+PHN0eWxlPi5he2ZpbGw6I2UwZTBlMDt9LmJ7ZmlsbDojYzRjNGM0O30uY3tmaWxsOm5vbmU7fTwvc3R5bGU+PC9kZWZzPjx0aXRsZT5hdmF0YXItMjwvdGl0bGU+PHBhdGggY2xhc3M9ImEiIGQ9Ik02MCwzMEEzMCwzMCwwLDEsMSwzMCwwLDMwLDMwLDAsMCwxLDYwLDMwIi8+PHBhdGggY2xhc3M9ImIiIGQ9Ik00MC4zNSwyNy4wOWE5LjgyLDkuODIsMCwxLDEtOS44MS05LjgzLDkuODIsOS44MiwwLDAsMSw5LjgxLDkuODMiLz48cGF0aCBjbGFzcz0iYyIgZD0iTTI5LjE3LDQwLjg1YTkuMjMsOS4yMywwLDAsMCwyLjcxLDAsMTEuOSwxMS45LDAsMCwxLTEuMzQuMDdBMTIuMzIsMTIuMzIsMCwwLDEsMjkuMTcsNDAuODVaIi8+PHBhdGggY2xhc3M9ImIiIGQ9Ik0zOS4yNSwzNy44M2ExMy43NywxMy43NywwLDAsMS03LjM3LDMsOS4yMyw5LjIzLDAsMCwxLTIuNzEsMCwxMy42NiwxMy42NiwwLDAsMS03LjM1LTNBMjEsMjEsMCwwLDAsMTAuMDYsNTIuMzdhMjkuOSwyOS45LDAsMCwwLDQwLjc2LS44QTIxLDIxLDAsMCwwLDM5LjI1LDM3LjgzWiIvPjwvc3ZnPg=="
               alt="pricipal_pic"
               className="from__the__pricipal__container__img"
             />
